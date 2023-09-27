a40bf7b0c3e45a4f331a867427107ecd1de6d3ea
jonmitten <jonmitten@gmail.com>
2023-09-27 10:59:24 -0700

    copy-pasted from book for troubleshooting... not working

--- cherrytree/interfaces/city.ts
+++ cherrytree/interfaces/city.ts
@@ -1,12 +1,11 @@
 interface CityData {
-    id: number;
-    name: string;
-    state: string;
-    country: string;
-    coord: {
-        lon: number;
-        lat: number;
-    };
+  id: number;
+  name: string;
+  state: string;
+  country: string;
+  coord: {
+    lon: number;
+    lat: number;
+  };
 }
-
 export default CityData;
--- cherrytree/pages/api/[name].ts
+++ cherrytree/pages/api/[name].ts
@@ -1,28 +1,24 @@
 import CityData from "@/interfaces/city";
 import { NextApiRequest, NextApiResponse } from "next";
-import cities from '@/lib/city.list.json';
+import cities from "@/lib/city.list.json";
 
 const Cities = cities as CityData[];
 const NUM_SUGGESTIONS = 5;
 
-function searchCities(value: string):CityData[] {
-    const matchingCities = Cities.filter(city =>
-        city.name.toLowerCase()
-        .startsWith(value.toLowerCase()))
-        .slice(0, NUM_SUGGESTIONS);
-    
-    return matchingCities;
+function searchCities(value: string): CityData[] {
+  const matchingCities = Cities.filter((city) => city.name.toLowerCase().startsWith(value.toLowerCase())).slice(
+    0,
+    NUM_SUGGESTIONS,
+  );
+  return matchingCities;
 }
 
-export default function handler({query: {name}}: NextApiRequest, res:NextApiResponse) { 
-
-    const cityName = Array.isArray(name) ? name.join('') : name;
-
-    // filter the list of cities to those whose names contain the given name
-    const filteredCities = cityName? searchCities(cityName) : [];
-
-    // Return the filtered list of cities as JSON
-    return res.json({
-        cities: filteredCities
-    })
+export default function handler({ query: { name } }: NextApiRequest, res: NextApiResponse) {
+  const cityName = Array.isArray(name) ? name.join("") : name;
+  // Filter the list of cities to those whose name contains the given name
+  const filteredCities = cityName ? searchCities(cityName) : [];
+  // Return the filtered list of cities as JSON
+  return res.json({
+    cities: filteredCities,
+  });
 }
