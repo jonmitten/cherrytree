41b8986bac6eda0d6489410f6b06407eb939453b
Jon Mitten <jonmitten@gmail.com>
2023-09-26 21:39:22 -0700

    adds SearchBox

---
+++ cherrytree/components/SearchBox.tsx
@@ -0,0 +1,9 @@
+export default function SearchBox() {
+    return (
+        <>
+            <input 
+            className="bg-gray-200 p-2 rounded-lg w-64" 
+            type="text" placeholder="City Name"/>
+        </>
+    )
+}
